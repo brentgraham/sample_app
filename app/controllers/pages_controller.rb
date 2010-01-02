cb86ed670d8c3aa67153633b6e9ddd7c070456fd
brentg <brent.graham@gmail.com>
2010-01-02 18:56:33 +0000

    added application template

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,16 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
+    @bodyid = "home"
   end
 
   def contact
+    @title = "Contact"
+    @bodyid = "contact"
   end
 
+  def about
+    @title = "About"
+    @bodyid = "about"  
+  end
 end
